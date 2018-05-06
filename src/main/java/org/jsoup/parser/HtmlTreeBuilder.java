3f93fd5ff40cb20dc245b493148e33cceb1f4a7c
Cedomir Igaly <cedomir.igaly@e-dama.net>
2018-05-06 09:12:53 +0200

    Normalizing attributes in HtmlTreeBuilder.insertEmpty.

--- src/main/java/org/jsoup/parser/HtmlTreeBuilder.java
+++ src/main/java/org/jsoup/parser/HtmlTreeBuilder.java
@@ -223,7 +223,7 @@ public class HtmlTreeBuilder extends TreeBuilder {
 
     Element insertEmpty(Token.StartTag startTag) {
         Tag tag = Tag.valueOf(startTag.name(), settings);
-        Element el = new Element(tag, baseUri, startTag.attributes);
+        Element el = new Element(tag, baseUri, settings.normalizeAttributes(startTag.attributes));
         insertNode(el);
         if (startTag.isSelfClosing()) {
             if (tag.isKnownTag()) {
